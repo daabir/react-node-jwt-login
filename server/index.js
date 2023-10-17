616991416348f9c6bb1e34131646c15ecbdc641f
daabir mubashir <darmubashir2@gmail.com>
2023-10-18 00:06:51 +0530

    initialize

---
+++ server/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+
+app.use(express.json());
+app.use(cors());
+dotenv.config();
+
+app.listen(4000, () => {
+    console.log("Listening at 4000")
+})
